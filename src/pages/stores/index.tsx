09b0e131c8577cc8bd3062b31654e56eef204923
nkc930604@gmail.com <nkc930604@gmail.com>
2024-01-25 17:15:45 +0900

    Feat: "complete infinite scroll"
    useIntersectionObserver를 사용한 무한스크롤 구현 완료

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,26 +1,55 @@
 import Loading from "@/components/Loading";
-import { StoreApiResponse } from "@/interface";
+import { StoreType } from "@/interface";
 import axios from "axios";
 import Image from "next/image";
 import { useInfiniteQuery, useQuery } from "react-query";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import Pagination from "@/components/Pagination";
+import { Fragment, useCallback, useEffect, useRef } from "react";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import Loader from "@/components/Loader";
+import { clear } from "console";
 
 export default function StoreListPage() {
-  const router = useRouter();
-  const { page = "1" }: any = router.query;
+  const ref = useRef<HTMLDivElement>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
+
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios(`/api/stores?page=${pageParam}`, {
+      params: { page: pageParam, limit: 10 },
+    });
+    return data;
+  };
 
   const {
     data: stores,
-    isLoading,
     isError,
-  } = useQuery(`stores=${page}`, async () => {
-    const { data } = await axios(`/api/stores?page=${page}`);
-    return data as StoreApiResponse;
+    isLoading,
+    isFetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery("stores", fetchStores, {
+    getNextPageParam: (lastPage: any) =>
+      lastPage.data.length > 0 ? lastPage.page + 1 : undefined,
   });
 
-  const result = useInfiniteQuery('stores')
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | undefined;
+    if (isPageEnd && hasNextPage) {
+      timer = setTimeout(() => {
+        fetchNext();
+      }, 500);
+    }
+    return () => clearTimeout(timer);
+  }, [fetchNextPage, hasNextPage, isPageEnd]);
 
   if (isError) {
     return (
@@ -29,48 +58,54 @@ export default function StoreListPage() {
       </div>
     );
   }
+
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
       <ul role="list" className="divide-y divide-gray-100">
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.data.map((store, index) => (
-            <li className="flex justify-between gap-x-6 py-5" key={index}>
-              <div className="flex gap-x-4 ">
-                <Image
-                  src={
-                    store?.category
-                      ? `/images/markers/${store?.category}.png`
-                      : "/images/markers/default.png"
-                  }
-                  alt="아이콘 이미지"
-                  width={48}
-                  height={48}
-                />
-                <div>
-                  <div className="text-sm font-semibold leading-9 text-gray-900">
-                    {store?.name}
+          stores?.pages?.map((page, index) => (
+            <Fragment key={index}>
+              {page.data.map((store: StoreType, i: any) => (
+                <li className="flex justify-between gap-x-6 py-5" key={i}>
+                  <div className="flex gap-x-4 ">
+                    <Image
+                      src={
+                        store?.category
+                          ? `/images/markers/${store?.category}.png`
+                          : "/images/markers/default.png"
+                      }
+                      alt="아이콘 이미지"
+                      width={48}
+                      height={48}
+                    />
+                    <div>
+                      <div className="text-sm font-semibold leading-9 text-gray-900">
+                        {store?.name}
+                      </div>
+                      <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                        {store?.name}
+                      </div>
+                    </div>
                   </div>
-                  <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                    {store?.name}
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <div className="text-sm font-semibold leading-9 text-gray-900">
+                      {store?.address}
+                    </div>
+                    <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                      {store?.phone || "번호없음"} |{store?.foodCertifyName} |
+                      {store?.category}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <div className="text-sm font-semibold leading-9 text-gray-900">
-                  {store?.address}
-                </div>
-                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                  {store?.phone || "번호없음"} |{store?.foodCertifyName} |
-                  {store?.category}
-                </div>
-              </div>
-            </li>
+                </li>
+              ))}
+            </Fragment>
           ))
         )}
       </ul>
-      {stores?.totalPage && <Pagination total={stores.totalPage} page={page} />}
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
     </div>
   );
 }
