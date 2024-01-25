ea2fe562672143c756f119b4b719c1981b6310bc
nkc930604@gmail.com <nkc930604@gmail.com>
2024-01-26 02:47:46 +0900

    Feat: "Add update store"
    맛집 등록 기능 추가

--- src/pages/stores/new.tsx
+++ src/pages/stores/new.tsx
@@ -1,7 +1,223 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+import { CATEGORY_ARR, FOOD_CERTIFY_ARR, STORE_TYPE_ARR } from "@/data/store";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
 export default function StoreNewPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Store New</h1>
-    </div>
+    <form
+      className="px-4 md:max-w-4xl mx-auto py-8"
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        try {
+          const res = await axios.post("/api/stores", data);
+          console.log(res);
+
+          if (res.status === 200) {
+            toast.success("맛집 등록에 성공했습니다.");
+            router.replace(`/stores/${res?.data?.id}`);
+          } else {
+            toast.error("맛집 등록에 실패했습니다.");
+          }
+        } catch (e) {
+          console.log(e);
+          toast.error("맛집 등록에 실패했습니다.");
+        }
+      })}
+    >
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            맛집 등록
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            아래 내용을 입력해서 맛집을 등록해주세요.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                가게명
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset outline-none px-2 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors?.name?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    가게명을 입력해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="category"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                카테고리
+              </label>
+              <div className="mt-2">
+                <select
+                  {...register("category", { required: true })}
+                  className="block w-full rounded-md border-0 px-2 py-2 outline-none text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">카테고리 선택</option>
+                  {CATEGORY_ARR.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+                {errors?.category?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    카테고리를 선택해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                연락처
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("phone", { required: true })}
+                  className="block w-full outline-none px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors?.phone?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    연락처를 입력해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="address"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                주소 (다음 주소 검색 API)
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register("address", { required: true })}
+                  className="block w-full rounded-md border-0 px-2 outline-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors?.address?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    연락처를 입력해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                식품인증구분
+              </label>
+              <div className="mt-2">
+                <select
+                  {...register("foodCertifyName", { required: true })}
+                  className="outline-none block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">식품인증구분 선택</option>
+                  {FOOD_CERTIFY_ARR.map((certify) => (
+                    <option key={certify} value={certify}>
+                      {certify}
+                    </option>
+                  ))}
+                </select>
+                {errors?.foodCertifyName?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    식품인증구분을 선택해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="storeType"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                업종구분
+              </label>
+              <div className="mt-2">
+                <select
+                  {...register("storeType", { required: true })}
+                  className="outline-none px-2 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">업종구분 선택</option>
+                  {STORE_TYPE_ARR.map((type) => (
+                    <option key={type} value={type}>
+                      {type}
+                    </option>
+                  ))}
+                </select>
+                {errors?.storeType?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    업종구분을 선택해주세요.
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          뒤로가기
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          제출하기
+        </button>
+      </div>
+    </form>
   );
 }
